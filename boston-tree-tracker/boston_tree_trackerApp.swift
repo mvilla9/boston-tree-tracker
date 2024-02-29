83c97a6b49bdd0661d6162b4e59325b2dbf0a13a
mvilla9 <mateo.john.villa@gmail.com>
2024-02-28 19:31:28 -0500

    Initial Commit

---
+++ boston-tree-tracker/boston_tree_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  boston_tree_trackerApp.swift
+//  boston-tree-tracker
+//
+//  Created by Mateo Villa on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct boston_tree_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
